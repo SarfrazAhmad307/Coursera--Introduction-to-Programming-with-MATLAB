ced1108ce787ffdc8c9c55f7a8e0afb7b38a2a30
SarfrazAhmad307 <Sarfaraz_076@outlook.com>
2020-05-24 05:01:39 +0500

    Week 2 assignments

---
+++ Week 2/Week_2_1.m
@@ -0,0 +1,4 @@
+Borrowed = 1000;
+Interest = 0.1;
+debt_1 = Borrowed*Interest+Borrowed;
+debt = debt_1*Interest+debt_1;
---
+++ Week 2/Week_2_2.m
@@ -0,0 +1,10 @@
+distance = 100;
+time = 9.58;
+distance_km = 100/1000;
+time_h = 9.58/3600;
+hundred = distance_km/time_h
+
+marathon_d = 42.195;
+marathon_time = 2*3600+1*60+39;
+marathon_newtime = marathon_time/3600;
+marathon = marathon_d/marathon_newtimeccl

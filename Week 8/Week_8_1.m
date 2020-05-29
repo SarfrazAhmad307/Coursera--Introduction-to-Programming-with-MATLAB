e04e45c03ac23572cef05b3862308c1eb647744b
SarfrazAhmad307 <Sarfaraz_076@outlook.com>
2020-05-29 05:05:04 +0500

    Week 8 Assignments

--- Week 8/Week_8_1.m
+++ Week 8/Week_8_1.m
@@ -1,10 +1,13 @@
-coded = caesar('ABCD',3);
+% Simple Encryption
+% Code to call your function
+coded    = caesar('ABCD',  3);
+decoded  = caesar(coded,  -3);
+wrap     = caesar('1234', 96);
+back     = caesar(wrap,  -96);
+
+% Function
 function coded = caesar(Char , Shift)
-    Double = Char + Shift;
-    coded = [];
-    for i = 1:length(Double)
-        if Double(i) >= 32 && Double(i) <= 126
-            coded = [coded,char(Double(i))];
-        end
-    end
-end
+   Shift = mod(Shift,95);
+   Double = Char + Shift;
+   coded = char(Double-(Double>126).*95+(Double<32).*95);
+end    
